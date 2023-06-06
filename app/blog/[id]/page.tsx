fd8df11987c7aad5b782e8c9e7ede17deb590760
ZubayerJS <zubayerjs.dev@gmail.com>
2023-06-06 12:56:51 +0600

    initial commit

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -26,16 +26,16 @@ export default async function SingleBlog({ params: { id } }: Props) {
 
     return (
         <div className="mx-4 my-10">
-            <h2 className="text-2xl font-bold text-indigo-700 p-3 ">{blog[0].title}</h2>
-            <div>
+            <h2 className="text-3xl font-bold text-indigo-800 py-3 ">{blog[0].title}</h2>
+            <div className="flex gap-3">
                 <img src={blog[0].authorImage} alt="authorProfile" className="w-12 h-12 object-cover rounded-full" />
 
                 <div>
-                    <h1>{blog[0].author}</h1>
-                    {/* <span>{Date(blog[0].createdAt).}</span> */}
+                    <h1 className="capitalize font-bold text-indigo-900">{blog[0].author}</h1>
+                    <span>{new Date(blog[0].createdAt).toDateString()}</span>
                 </div>
             </div>
-            <p className="text-gray-600 font-medium mt-10">{blog[0].content}</p>
+            <p className="text-gray-600 font-medium mt-10 text-lg">{blog[0].content}</p>
         </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -22,25 +22,27 @@ export default async function Home() {
   const data = await fetchBlogPost();
 
   return (
-    <main className="">
+    <>
       <NavBar />
-      <div className="grid md:grid-cols-3 gap-5 mx-10 my-10">
-        {data.map((data: Blog) => (
-          <div key={data.id} className="p-2 md:p-4 border-2 rounded-md hover:scale-105 hover:transition-all">
-            <Link href={`/blog/${data.id}`} className="font-bold">{data.title}</Link>
-            <div className="">{data.content.length > 100 ? data.content.slice(0, 100) : data.content} <span className="text-gray-400">...</span></div>
+      <main className="max-w-7xl md:mx-auto mx-2">
+        <div className="grid md:grid-cols-3 gap-4 mx-5 md:mx-10 my-10">
+          {data.map((data: Blog) => (
+            <div key={data.id} className="p-2 md:p-4 border-2 rounded-md hover:scale-105 hover:transition-all">
+              <Link href={`/blog/${data.id}`} className="font-bold">{data.title}</Link>
+              <div className="">{data.content.length > 100 ? data.content.slice(0, 100) : data.content} <span className="text-gray-400">...</span></div>
 
-            <div className="flex flex-col lg:flex-row justify-start lg:justify-between items-start gap-2 lg:items-center mt-2">
-              <span className="border border-indigo-400 bg-indigo-400 text-white font-bold uppercase p-1 rounded-md text-xs transition-all">{data.category}</span>
+              <div className="flex flex-col lg:flex-row justify-start lg:justify-between items-start gap-2 lg:items-center mt-2">
+                <span className="border border-indigo-400 bg-indigo-400 text-white font-bold uppercase p-1 rounded-md text-xs transition-all">{data.category}</span>
 
-              <div className="flex justify-end gap-2 items-center">
-                <img src={data.authorImage} alt="authorImage" className="w-8 h-8 object-cover rounded-full" />
-                <p className="font-semibold text-xs lg:text-sm capitalize">{data.author}</p>
+                <div className="flex justify-end gap-2 items-center">
+                  <img src={data.authorImage} alt="authorImage" className="w-8 h-8 object-cover rounded-full" />
+                  <p className="font-semibold text-xs lg:text-sm capitalize">{data.author}</p>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
-    </main>
+          ))}
+        </div>
+      </main>
+    </>
   );
 }
