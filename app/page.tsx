3c1eee9f11ce5d136eb86fcbc111ea5bc14fcb1c
mahfuz68 <mymsicret34@gmail.com>
2023-06-07 16:57:05 +0600

    fix some error

--- app/page.tsx
+++ app/page.tsx
@@ -31,44 +31,38 @@ export default async function Home() {
       <main className="max-w-7xl md:mx-auto mx-2">
         <div className="grid md:grid-cols-3 gap-4 mx-5 md:mx-10 my-10">
           {data.map((data: Blog) => {
-            const imgx = data.authorImage.split(
-              "https://images.unsplash.com"
-            )[1];
-
             return (
-              <div
-                key={data.id}
-                className="p-2 md:p-4 border-2 rounded-md hover:scale-105 hover:transition-all"
-              >
-                <Link href={`/blog/${data.id}`} className="font-bold">
-                  {data.title}
-                </Link>
-                <div className="">
-                  {data.content.length > 100
-                    ? data.content.slice(0, 100)
-                    : data.content}{" "}
-                  <span className="text-gray-400">...</span>
-                </div>
+              <Link key={data.id} href={`/${data.id}`}>
+                <div className="p-2 md:p-4 border-2 rounded-md hover:scale-105 hover:transition-all">
+                  <h1 className="font-bold">{data.title}</h1>
 
-                <div className="flex flex-col lg:flex-row justify-start lg:justify-between items-start gap-2 lg:items-center mt-2">
-                  <span className="border border-indigo-400 bg-indigo-400 text-white font-bold uppercase p-1 rounded-md text-xs transition-all">
-                    {data.category}
-                  </span>
+                  <div className="">
+                    {data.content.length > 100
+                      ? data.content.slice(0, 100)
+                      : data.content}{" "}
+                    <span className="text-gray-400">...</span>
+                  </div>
 
-                  <div className="flex justify-end gap-2 items-center">
-                    <Image
-                      src={data.authorImage}
-                      width={480}
-                      height={500}
-                      alt="authorImage"
-                      className="w-8 h-8 object-cover rounded-full"
-                    />
-                    <p className="font-semibold text-xs lg:text-sm capitalize">
-                      {data.author}
-                    </p>
+                  <div className="flex flex-col lg:flex-row justify-start lg:justify-between items-start gap-2 lg:items-center mt-2">
+                    <span className="border border-indigo-400 bg-indigo-400 text-white font-bold uppercase p-1 rounded-md text-xs transition-all">
+                      {data.category}
+                    </span>
+
+                    <div className="flex justify-end gap-2 items-center">
+                      <Image
+                        src={data.authorImage}
+                        width={480}
+                        height={500}
+                        alt="authorImage"
+                        className="w-8 h-8 object-cover rounded-full"
+                      />
+                      <p className="font-semibold text-xs lg:text-sm capitalize">
+                        {data.author}
+                      </p>
+                    </div>
                   </div>
                 </div>
-              </div>
+              </Link>
             );
           })}
         </div>
