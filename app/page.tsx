c1df6b94e788e638934dde0a89615c746fe7ed1f
mahfuz68 <mymsicret34@gmail.com>
2023-06-07 16:01:25 +0600

    image tag added and error fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import NavBar from "@/components/NavBar";
+import Image from "next/image";
 import Link from "next/link";
 
-// export func
 interface Blog {
   id: number;
   title: string;
@@ -12,9 +12,11 @@ interface Blog {
   createdAt: number;
 }
 
+export const dynamic = "force-static";
+
 const fetchBlogPost = async () => {
   const response = await fetch("http://localhost:3000/api/blog", {
-    next: { revalidate: 60 },
+    cache: "force-cache",
   });
   const data = await response.json();
   return data;
@@ -28,39 +30,47 @@ export default async function Home() {
       <NavBar />
       <main className="max-w-7xl md:mx-auto mx-2">
         <div className="grid md:grid-cols-3 gap-4 mx-5 md:mx-10 my-10">
-          {data.map((data: Blog) => (
-            <div
-              key={data.id}
-              className="p-2 md:p-4 border-2 rounded-md hover:scale-105 hover:transition-all"
-            >
-              <Link href={`/blog/${data.id}`} className="font-bold">
-                {data.title}
-              </Link>
-              <div className="">
-                {data.content.length > 100
-                  ? data.content.slice(0, 100)
-                  : data.content}{" "}
-                <span className="text-gray-400">...</span>
-              </div>
+          {data.map((data: Blog) => {
+            const imgx = data.authorImage.split(
+              "https://images.unsplash.com"
+            )[1];
 
-              <div className="flex flex-col lg:flex-row justify-start lg:justify-between items-start gap-2 lg:items-center mt-2">
-                <span className="border border-indigo-400 bg-indigo-400 text-white font-bold uppercase p-1 rounded-md text-xs transition-all">
-                  {data.category}
-                </span>
+            return (
+              <div
+                key={data.id}
+                className="p-2 md:p-4 border-2 rounded-md hover:scale-105 hover:transition-all"
+              >
+                <Link href={`/blog/${data.id}`} className="font-bold">
+                  {data.title}
+                </Link>
+                <div className="">
+                  {data.content.length > 100
+                    ? data.content.slice(0, 100)
+                    : data.content}{" "}
+                  <span className="text-gray-400">...</span>
+                </div>
 
-                <div className="flex justify-end gap-2 items-center">
-                  <img
-                    src={data.authorImage}
-                    alt="authorImage"
-                    className="w-8 h-8 object-cover rounded-full"
-                  />
-                  <p className="font-semibold text-xs lg:text-sm capitalize">
-                    {data.author}
-                  </p>
+                <div className="flex flex-col lg:flex-row justify-start lg:justify-between items-start gap-2 lg:items-center mt-2">
+                  <span className="border border-indigo-400 bg-indigo-400 text-white font-bold uppercase p-1 rounded-md text-xs transition-all">
+                    {data.category}
+                  </span>
+
+                  <div className="flex justify-end gap-2 items-center">
+                    <Image
+                      src={data.authorImage}
+                      width={480}
+                      height={500}
+                      alt="authorImage"
+                      className="w-8 h-8 object-cover rounded-full"
+                    />
+                    <p className="font-semibold text-xs lg:text-sm capitalize">
+                      {data.author}
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            );
+          })}
         </div>
       </main>
     </>
--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["images.unsplash.com"],
+    // remotePatterns: [
+    //   {
+    //     protocol: "https",
+    //     hostname: "images.unsplash.com",
+    //     port: "",
+    //     pathname: "/**",
+    //   },
+    // ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
